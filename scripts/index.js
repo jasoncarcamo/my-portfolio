77d8a21d7e1bc480af69506b768278c9ad4efe7e
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-02-01 16:40:36 -0500

    Changed landing page layout

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,24 @@
 let screenWidth = window.innerWidth;
-const menuBurger = document.getElementById("menu-burger");
-const navLinks = document.getElementById("nav-links");
-const closeMenuButton = document.getElementById("close-burger-menu");
 
 function handleScreenSize(){
     window.addEventListener("resize", ()=>{
         screenWidth = window.innerWidth;
     });
+};
+
+function redirect(){
+    const buttons = document.querySelectorAll("#home-section > button");
+    console.log(buttons);
+
+    Array.from(buttons).forEach( (button, index)=>{
+        button.addEventListener("click", (e)=>{
+            console.log(e.target.children)
+            window.location.href = e.target.children[0].pathname;
+        })
+    })
 }
 
-function handleMenuBurger(){
-    menuBurger.addEventListener("click", ()=>{
-        menuBurger.classList.toggle("hide-menu-burger");
-        closeMenuButton.classList.toggle("show-close-menu");
-        navLinks.classList.toggle("display-links");
-    });
-}
 
-function closeBurgermenu(){
-    closeMenuButton.addEventListener("click", ()=>{
-        menuBurger.classList.toggle("hide-menu-burger");
-        closeMenuButton.classList.toggle("show-close-menu");
-        navLinks.classList.toggle("display-links");
-    });
-}
-
-function navLinksScroll(){
-    navLinks.addEventListener("touchmove", (e)=>{
-        e.preventDefault();
-    });
-}
 
 handleScreenSize();
-handleMenuBurger();
-closeBurgermenu();
-navLinksScroll();
+redirect();
