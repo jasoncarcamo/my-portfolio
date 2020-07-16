865263e68cd021c401dc897d13d609a693a6353c
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2020-07-16 00:41:23 -0400

    Added new design skeleton

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-function learnMore(){
-    const button = document.getElementById("learn-more-btn");
-
-    button.addEventListener("click", (e)=>{
-        window.location.href = "https://jasoncarcamo.github.io/my-portfolio/about.html"
-    });
-};
-
-learnMore();
