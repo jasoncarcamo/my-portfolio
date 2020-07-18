54083ab314a9bfd53a2b8e7d8f76ab8e6aaba5d6
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-07-17 23:42:20 -0400

    Added animation to skill section

--- scripts/projects.js
+++ scripts/projects.js
@@ -228,13 +228,14 @@ $(function(){
     function isInViewport(element) {
         var rect = element.getBoundingClientRect();
         var html = document.documentElement;
+        
         return (
-          rect.top >= 0 &&
-          rect.left <= 0 &&
-          rect.bottom <= (window.innerHeight || html.clientHeight) &&
-          rect.right <= (window.innerWidth || html.clientWidth)
+            rect.top >= 0 &&
+            rect.left <= 0 &&
+            rect.bottom <= (window.innerHeight || html.clientHeight) &&
+            rect.right <= (window.innerWidth || html.clientWidth)
         );
-      }
+    }
     
     addFrontProjects();
     addBackendProjects();
---
+++ scripts/skills.js
@@ -0,0 +1,33 @@
+$(function(){
+    function popSkills(){
+        const skillsSection = document.querySelectorAll("#skills > section");
+
+        Array.from(skillsSection).forEach((section, index)=>{
+            if(isInViewport(section)){
+                skillsSection[index].classList.add("pop");
+            };
+        });
+
+        window.addEventListener("scroll", (e)=>{
+            Array.from(skillsSection).forEach((skill, index)=>{
+                if(isInViewport(skill)){
+                    skillsSection[index].classList.add("pop");
+                };
+            });
+        });
+    };
+
+    function isInViewport(element) {
+        var rect = element.getBoundingClientRect();
+        var html = document.documentElement;
+
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || html.clientHeight) &&
+            rect.right <= (window.innerWidth || html.clientWidth)
+        );
+    };
+
+    popSkills();
+});
