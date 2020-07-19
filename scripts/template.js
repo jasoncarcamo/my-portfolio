6a1978bb92226ccbbfdcf8c7ba7d64d57c3312c1
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2020-07-18 23:15:28 -0400

    Fixed safari animation bug

--- scripts/template.js
+++ scripts/template.js
@@ -6,6 +6,7 @@ const closeMenuButton = document.getElementById("close-burger-menu");
 let screenWidth = window.innerWidth;
 const scrollY = window.scrollY;
 const navHeader = document.getElementById("nav-header");
+const navAnchors = document.querySelectorAll("#nav-links a");
 
 function navBarAnimation(){
     let prevYOffset = window.pageYOffset;
@@ -45,6 +46,17 @@ function handleMenuBurger(){
         return;
     };
 
+    if(screenWidth < 1400){
+        Array.from(navAnchors).forEach((link, index)=>{
+            link.addEventListener("click", (e)=>{
+                document.querySelector("#nav-bar-container > div").classList.toggle("fade");
+                document.querySelector("#nav-bar-container").classList.toggle("show-container")
+                menuBurger.classList.toggle("is-active");
+                navLinks.classList.toggle("display-links");
+            });
+        })
+    }
+
     menuBurger.addEventListener("click", ()=>{
         document.querySelector("#nav-bar-container > div").classList.toggle("fade");
         document.querySelector("#nav-bar-container").classList.toggle("show-container")
@@ -57,7 +69,8 @@ function closeBurgermenu(){
     if(!closeMenuButton){
 
         return;
-    }
+    };
+
     closeMenuButton.addEventListener("click", ()=>{
         menuBurger.classList.toggle("hide-menu-burger");
         closeMenuButton.classList.toggle("show-close-menu");
