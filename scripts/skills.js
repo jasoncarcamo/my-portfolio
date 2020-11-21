8a501ab655464cff6a55dd0516c83550c1c888fd
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2020-11-20 22:21:23 -0800

    Quick commit

--- scripts/skills.js
+++ scripts/skills.js
@@ -15,17 +15,5 @@ $(function(){
         });
     };
 
-    function isInViewport(element) {
-        var rect = element.getBoundingClientRect();
-        var html = document.documentElement;
-
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || html.clientHeight) &&
-            rect.right <= (window.innerWidth || html.clientWidth)
-        );
-    };
-
     popSkills();
 });
