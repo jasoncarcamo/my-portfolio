216ff75c22cba5e5f8395cb7f023b206e8e8f411
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2020-12-08 18:39:28 -0800

    Quick commit

--- scripts/skills.js
+++ scripts/skills.js
@@ -1,5 +1,6 @@
 $(function(){
     function popSkills(){
+        
         const frontendSkills = document.getElementById("frontend-skills");
         const backendSkills = document.getElementById("backend-skills");
         const tools = document.getElementById("development-tools");
